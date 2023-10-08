1758e7b29aa6ed2d52db134466908a792bf79079
Zegionnn <muhammadibad024@gmail.com>
2023-10-08 19:50:34 +0700

    Tugas Baru

---
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author Ibad
+ */
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("23082010169");
+        System.out.println("Muhammad Irsyadul Ibad");
+    }
+    
+}
